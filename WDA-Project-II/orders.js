e3211886364f0432a78d1ab2774d4bde93cf97cb
AmanIIITL <amanbvbpn0152@gmail.com>
2025-02-19 14:39:36 +0530

    Add files via upload

---
+++ WDA-Project-II/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        CoursePrice: '₹1299',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        CoursePrice: '₹999',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        CoursePrice: '₹199',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
